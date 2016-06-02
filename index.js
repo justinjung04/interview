36f9d141f6566f871d4c4e7292c8526ea2413fd2
Justin Jung <justinjung04@gmail.com>
2016-06-02 12:05:53 -0700

    merge sort done

--- index.js
+++ index.js
@@ -6,12 +6,13 @@ Quick Sort - O(n log n) ~ O(n^2)
 Merge Sort - O(n log n)
 */
 
-const baseArray = [7, 4, 2, 5, 8, 9, 12, 3, 1, 6, 11, 10];
+const baseArray = [7, 4, 2, 5, 8, 9, 11, 3, 1, 6, 10];
 
 const swap = (array, index1, index2) => {
 	if(index1 == index2) {
 		return;
 	}
+	
 	const tmp = array[index1];
 	array[index1] = array[index2];
 	array[index2] = tmp;
@@ -20,31 +21,37 @@ const swap = (array, index1, index2) => {
 const bubbleSort = array => {
 	for(let i=0; i<array.length; i++) {
 		let swapCount = 0;
+
 		for(let j=0; j<array.length-i; j++) {
 			if(array[j] > array[j+1]) {
 				swap(array, j, j+1);
 				swapCount++;
 			}
 		}
+
 		if(swapCount == 0) {
 			break;
 		}
 	}
+
 	console.log(array);
 };
 
 const selectionSort = array => {
 	for(let i=0; i<array.length; i++) {
 		let minIndex = i;
+
 		for(let j=i+1; j<array.length; j++) {
 			if(array[j] < array[minIndex]) {
 				minIndex = j;
 			}
 		}
+
 		if(i !== minIndex) {
 			swap(array, i, minIndex);
 		}
 	}
+
 	console.log(array);
 };
 
@@ -56,6 +63,7 @@ const insertionSort = array => {
 			}
 		}
 	}
+
 	console.log(array);
 };
 
@@ -89,12 +97,41 @@ const quickSort = (array) => {
 };
 
 const mergeSort = array => {
-	const merge = (sub1, sub2) => {
-		const newArray = [];
-		for(let i=0; i<sub1.length+sub2.length-1; i++) {
+	let output = [];
 
+	for(let i=0; i<array.length; i++) {
+		output.push([array[i]]);
+	}
+
+	const merge = (arr1, arr2) => {
+		const sum = [];
+		let cnt1 = 0;
+		let cnt2 = 0;
+		for(let i=0; i<arr1.length + arr2.length; i++) {
+			if(cnt2 >= arr2.length || arr1[cnt1] < arr2[cnt2]) {
+				sum.push(arr1[cnt1]);
+				cnt1++;
+			} else {
+				sum.push(arr2[cnt2]);
+				cnt2++;
+			}
 		}
+		return sum;
 	};
+
+	while(output.length > 1) {
+		const tmp = [];
+		for(let i=0; i<output.length; i += 2) {
+			if(i + 1 == output.length) {
+				tmp.push(output[i]);
+			} else {
+				tmp.push(merge(output[i], output[i + 1]));
+			}
+		}
+		output = tmp;
+	}
+
+	console.log(output[0]);
 };
 
 window.onload = () => {
@@ -102,4 +139,5 @@ window.onload = () => {
 	selectionSort(baseArray.slice());
 	insertionSort(baseArray.slice());	
 	quickSort(baseArray.slice());
+	mergeSort(baseArray.slice());
 }
