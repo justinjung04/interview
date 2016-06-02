412fb1646a49e89182a226c97a1f6f0d6168f5c7
Justin Jung <justinjung04@gmail.com>
2016-06-02 10:41:31 -0700

    insertion + quick sort done

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 /* 
-Bubble Sort - O(N^2)
-Selection Sort - O(N^2)
-Insertion Sort - O(N^2)
-Quick Sort - O(N^2) ~ O(Nlog(N))
-Merge Sort - O(Nlog(N))
+Bubble Sort - O(n^2)
+Selection Sort - O(n^2)
+Insertion Sort - O(n^2)
+Quick Sort - O(n log n) ~ O(n^2)
+Merge Sort - O(n log n)
 */
 
 const baseArray = [7, 4, 2, 5, 8, 9, 12, 3, 1, 6, 11, 10];
 
+const swap = (array, index1, index2) => {
+	if(index1 == index2) {
+		return;
+	}
+	const tmp = array[index1];
+	array[index1] = array[index2];
+	array[index2] = tmp;
+};
+
 const bubbleSort = array => {
-	for(let i=0; i<array.length-1; i++) {
+	for(let i=0; i<array.length; i++) {
 		let swapCount = 0;
-		for(let j=0; j<array.length-1-i; j++) {
+		for(let j=0; j<array.length-i; j++) {
 			if(array[j] > array[j+1]) {
-				const tmp = array[j];
-				array[j] = array[j+1];
-				array[j+1] = tmp;
+				swap(array, j, j+1);
 				swapCount++;
 			}
 		}
@@ -27,25 +34,63 @@ const bubbleSort = array => {
 };
 
 const selectionSort = array => {
-	for(let i=0; i<array.length-1; i++) {
+	for(let i=0; i<array.length; i++) {
 		let minIndex = i;
-		for(let j=i+1; j<array.length-1; j++) {
+		for(let j=i+1; j<array.length; j++) {
 			if(array[j] < array[minIndex]) {
 				minIndex = j;
 			}
 		}
 		if(i !== minIndex) {
-			const tmp = array[i];
-			array[i] = array[minIndex];
-			array[minIndex] = tmp;
+			swap(array, i, minIndex);
 		}
 	}
 	console.log(array);
 };
 
-const mergeSort = array => {
+const insertionSort = array => {
+	for(let i=0; i<array.length; i++) {
+		for(let j=i+1; j>0; j--) {
+			if(array[j] < array[j-1]) {
+				swap(array, j, j-1);
+			}
+		}
+	}
+	console.log(array);
+};
 
+const quickSort = (array) => {
+	let start = 0;
+	let end = array.length - 1;
+	let iterations = 0;
+
+	const partition = (array, start, end) => {
+		let index = start - 1;
+		
+		for(let i=start; i<=end; i++) {
+			if(array[i] < array[end]) {
+				index++;
+				swap(array, i, index);
+			} else if(i == end) {
+				index++;
+				swap(array, end, index);
+			}
+		}
+
+		if(index > start + 1) {
+			partition(array, start, index - 1);
+		} else if(index < end - 1) {
+			partition(array, index + 1, end);
+		}
+	};
+
+	partition(array, start, end);
+	console.log(array);
+};
+
+const mergeSort = array => {
 	const merge = (sub1, sub2) => {
+		const newArray = [];
 		for(let i=0; i<sub1.length+sub2.length-1; i++) {
 
 		}
@@ -53,6 +98,8 @@ const mergeSort = array => {
 };
 
 window.onload = () => {
-	bubbleSort(baseArray);
-	selectionSort(baseArray);
+	bubbleSort(baseArray.slice());
+	selectionSort(baseArray.slice());
+	insertionSort(baseArray.slice());	
+	quickSort(baseArray.slice());
 }
